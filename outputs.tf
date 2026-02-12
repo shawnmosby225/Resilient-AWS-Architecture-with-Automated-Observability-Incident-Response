817760f9f35e0b67ac28f1fac2f8e0a57caeb726
Shawn Mosby <shawnmosby225@gmail.com>
2026-02-11 22:38:50 -0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -35,4 +35,27 @@ output "chewbacca_waf_cw_log_group_name" {
    value = var.waf_log_destination == "cloudwatch" ? aws_cloudwatch_log_group.chewbacca_waf_log_group01[0].name : null 
 }
 
+#Bonus-A outputs (append to outputs.tf)
+
+# Explanation: These outputs prove Chewbacca built private hyperspace lanes (endpoints) instead of public chaos.
+output "chewbacca_vpce_ssm_id" {
+  value = aws_vpc_endpoint.chewbacca_vpce_ssm01.id
+}
+
+output "chewbacca_vpce_logs_id" {
+  value = aws_vpc_endpoint.chewbacca_vpce_logs01.id
+}
+
+output "chewbacca_vpce_secrets_id" {
+  value = aws_vpc_endpoint.chewbacca_vpce_secrets01.id
+}
+
+output "chewbacca_vpce_s3_id" {
+  value = aws_vpc_endpoint.chewbacca_vpce_s3_gw01.id
+}
+
+output "chewbacca_private_ec2_instance_id_bonus" {
+  value = aws_instance.chewbacca_ec201_private_bonus.id
+}
+
 
